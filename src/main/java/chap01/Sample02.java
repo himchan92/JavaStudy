9cff727f8731c9a927ec77cba9f625f57205692f
omea92 <thdjun92@naver.com>
2025-02-03 02:18:00 +0900

    chap01 > Sample02 실습 > 출력문

--- src/main/java/chap01/Sample02.java
+++ src/main/java/chap01/Sample02.java
@@ -1,7 +1,12 @@
 package chap01;
 
 public class Sample02 {
-
+    
+    /*
+        출력문 
+        - println : 줄바꿈 출력
+        - print : 줄바꿈 x 출력
+     */
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println(" World");
